27e28162d7c2a28d77e83fc3b713fc7fa0bf48a4
Gamma_Sigma <lateo1234@gmail.com>
2024-03-22 21:55:27 +0100

    Animation Test: improvements
    - `m_time_interval_s`: missing floating point `f` suffix after `1.0`.
    - `m_ellipseRadius` added to avoid hard-coding of values (magic numbers).
    - `m_rotationRadius` renamed for clarity (it was `m_radius`).
    - Removed a bunch of `(float)` C-style casts, because they were unnecessary (implied conversions are used).
    - `sin` and `cos` functions reversed to replicate the actual decomposition of a rotation on the x, y axes. Warning: rotation direction reversed because the y axis usually points downwards in graphics programming.

--- experiments/Animation_Test/Source/MainComponent.cpp
+++ experiments/Animation_Test/Source/MainComponent.cpp
@@ -8,8 +8,8 @@ MainComponent::MainComponent(void)
   // Make sure you set the size of the component after you add any child components.
   setSize(800, 600);
 
-  // setFramesPerSecond(m_frames_per_second); // This sets the frequency of the update calls.
-  setSynchroniseToVBlank(true); // (GS) used to reduce the run-time stuttering of the animation. Replaces the previous instruction.
+  setFramesPerSecond(m_frames_per_second); // This sets the frequency of the update calls.
+  // setSynchroniseToVBlank(true); // (GS) used to reduce the run-time stuttering of the animation. Replaces the previous instruction.
 }
 
 MainComponent::~MainComponent(void)
@@ -20,11 +20,11 @@ void MainComponent::update(void)
 {
   // This function is called at the frequency specified by the `setFramesPerSecond()` call
   // in the constructor. You can use it to update counters, animate values, etc.
-  const float x_centering = (float)getWidth()  / 2.0f;
-  const float y_centering = (float)getHeight() / 2.0f;
+  const float x_centering = getWidth()  / 2.0f;
+  const float y_centering = getHeight() / 2.0f;
   const float currentTime = getFrameCounter() * m_time_interval_s;
-  m_xCoord  = x_centering + (float)m_radius * std::sin((float) currentTime * m_two_pi * m_frequency_hz);
-  m_yCoord  = y_centering + (float)m_radius * std::cos((float) currentTime * m_two_pi * m_frequency_hz);
+  m_xCoord  = x_centering + m_rotationRadius * std::cos(currentTime * m_two_pi * m_frequency_hz); // Warning: rotation direction reversed (clockwise instead of counter-clockwise) because the y axis usually points downwards in graphics programming.
+  m_yCoord  = y_centering + m_rotationRadius * std::sin(currentTime * m_two_pi * m_frequency_hz);
 }
 
 //==============================================================================
@@ -37,7 +37,7 @@ void MainComponent::paint (juce::Graphics& g)
   g.setColour(getLookAndFeel().findColour(juce::Slider::thumbColourId));
   juce::Point<float> p(m_xCoord, m_yCoord);
 
-  g.fillEllipse(p.x, p.y, 30.0f, 30.0f);
+  g.fillEllipse(p.x, p.y, m_ellipseRadius, m_ellipseRadius);
 }
 
 void MainComponent::resized()
--- experiments/Animation_Test/Source/MainComponent.h
+++ experiments/Animation_Test/Source/MainComponent.h
@@ -25,10 +25,11 @@ private:
   // Your private member variables go here...
 
   static constexpr  int    m_frames_per_second = 60;
-  static constexpr  float  m_time_interval_s   = 1.0 / m_frames_per_second;
+  static constexpr  float  m_time_interval_s   = 1.0f / m_frames_per_second;
   static constexpr  float  m_two_pi            = 6.2832f;
   static constexpr  float  m_frequency_hz      = 0.5f;
-  static constexpr  int    m_radius            = 150;
+  static constexpr  int    m_ellipseRadius     = 30;
+  static constexpr  int    m_rotationRadius    = 150;
                     float  m_xCoord;
                     float  m_yCoord;
 
